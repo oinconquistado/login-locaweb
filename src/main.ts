8c5187129fb26941b45bad313bd372690070cdff
Neto <ssneto88@gmail.com>
2023-04-14 09:28:04 -0300

    fix(layout): updated CSS variables and basic.css renamed to base.css

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
-import { createApp } from 'vue'
-import './style/reset.css'
-import './style/basic.css'
+import { createApp } from "vue";
+import "./style/reset.css";
+import "./style/base.css";
 
-import App from './App.vue'
+import App from "./App.vue";
 
-createApp(App).mount('#app')
+createApp(App).mount("#app");
