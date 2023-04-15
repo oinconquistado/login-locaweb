1e90c58bf8b1eb8c9ad40208f890c74cf95d4f4c
Neto <ssneto88@gmail.com>
2023-04-15 13:51:58 -0300

    refact(button): standardization in the definition of defaults 🔨

--- src/components/button/controller/ButtonController.ts
+++ src/components/button/controller/ButtonController.ts
@@ -2,35 +2,6 @@ import * as Types from "../interface/ButtonInterface";
 import validateButtonProps from "../utils/buttonValidateProps";
 import treatButtonProps from "../utils/buttontSetDefaultProps";
 
-// Definindo as medidas para cada tamanho de botão
-const buttonMetrics: Types.ButtonMetrics = {
-  // Tamanho: "xs"
-  xs: {
-    height: "56px",
-    width: "245px",
-  },
-  // Tamanho: "sm"
-  sm: {
-    height: "56px",
-    width: "248px",
-  },
-  // Tamanho: "md"
-  md: {
-    height: "60px",
-    width: "333px",
-  },
-  // Tamanho: "lg"
-  lg: {
-    height: "60px",
-    width: "551px",
-  },
-  // Tamanho: "xl"
-  xl: {
-    height: "80px",
-    width: "551px",
-  },
-};
-
 // Função principal para inicializar o botão
 const initButton = async (props: Types.ButtonProps) => {
   validateButtonProps(props);
@@ -38,4 +9,3 @@ const initButton = async (props: Types.ButtonProps) => {
 };
 
 export default initButton;
-export { buttonMetrics };
