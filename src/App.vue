4a51daf0c55fd100bb5ffda49b50370a418b9ca1
Neto <ssneto88@gmail.com>
2023-04-15 14:54:34 -0300

    chore(assets): deleted unsed file

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,21 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+  import { ref } from "vue";
+  import Button from "./components/button/Button.vue";
+  import Input from "./components/input/Input.vue";
+
+  const message = ref("");
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <Button button_msg="Machado 98" variant="cta" size="xs"></Button>
+  <Input
+    label="E-mail"
+    v-model="message"
+    inputType="text"
+    placeholder="Insira seu e-mail"
+    description="Insira seu e-mail para receber as novidades"
+    size="mobile"
+  />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<style scoped></style>
