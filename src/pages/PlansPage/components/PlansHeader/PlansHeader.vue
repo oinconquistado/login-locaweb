6823f08c61b3c80231f15be6525321a3a4734645
Neto <ssneto88@gmail.com>
2023-04-17 18:05:05 -0300

    feat(plans-page): plans header ✨

---
+++ src/pages/PlansPage/components/PlansHeader/PlansHeader.vue
@@ -0,0 +1,30 @@
+<scrip lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  setup() {
+    return {};
+  },
+});
+</scrip>
+<template>
+  <div class="container__header">
+    <div class="container__logo-plans--logobox">
+      <img
+        src="../../../../assets/images/locaweb.svg"
+        class="container__logo-plans--logobox--locaweb"
+        alt="Logo da Locaweb"
+      />
+    </div>
+
+    <!-- chamadas do principal -->
+    <div class="plans_call">
+      <h1>Você está muito próximo de mudar a forma de</h1>
+      <h2 class="plans_call--host-your-site">hospedar seu site</h2>
+      <h2 class="plans_call--choose-your-plan--text">Escolha o seu plano</h2>
+    </div>
+  </div>
+</template>
+<style scoped>
+  @import url("./style/PlansHeaderStyle.css");
+</style>
