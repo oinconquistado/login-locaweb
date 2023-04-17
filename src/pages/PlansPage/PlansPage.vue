4466262a75ce36df7575940ed25aaf52c03ec996
Neto <ssneto88@gmail.com>
2023-04-17 18:05:55 -0300

    feat(plans-page): implemented Plans Page ✨

---
+++ src/pages/PlansPage/PlansPage.vue
@@ -0,0 +1,25 @@
+<script lang="ts">
+  import { defineComponent } from "vue";
+  import PlansHeader from "./components/PlansHeader/PlansHeader.vue";
+  import PlanContainer from "./components/PlanContainer/PlanContainer.vue";
+
+  export default defineComponent({
+    // declaração dos componentes
+    components: {
+      PlansHeader,
+      PlanContainer,
+    },
+    setup() {
+      return {};
+    },
+  });
+</script>
+
+<template>
+  <div class="container__logo-plans">
+    <!-- header principal -->
+    <PlansHeader />
+    <!-- container dos planos -->
+    <PlanContainer />
+  </div>
+</template>
