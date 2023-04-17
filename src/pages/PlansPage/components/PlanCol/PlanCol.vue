d2824bbb4b5a81b8fff61b5902bbbd259e45ac82
Neto <ssneto88@gmail.com>
2023-04-17 18:06:42 -0300

    feat(plans-page): Plan column component implemented ✨

---
+++ src/pages/PlansPage/components/PlanCol/PlanCol.vue
@@ -0,0 +1,146 @@
+<script lang="ts">
+  import { defineComponent } from "vue";
+  import Button from "../../../../components/button/Button.vue";
+  import { Plan } from "./interface/PlanColInterfaces";
+
+  export default defineComponent({
+    // declaração dos componentes
+    components: {
+      Button,
+    },
+    props: {
+      // propriedade que recebe os dados do plano
+      sections: {
+        type: Object as () => Plan,
+        required: true,
+      },
+    },
+    setup() {
+      return {};
+    },
+  });
+</script>
+
+<template>
+  <div class="plan_column">
+    <!-- header -->
+    <div class="plan_column--header">
+      <!-- títulos da coluna do plano -->
+      <div class="header-titles">
+        <h3 class="plan_title">{{ sections.plan_details.title }}</h3>
+        <div class="plan_price_box">
+          <h1
+            :class="[
+              sections.plan_details.price.toLowerCase() === 'grátis'
+                ? 'plan-free'
+                : 'plan-price',
+            ]"
+          >
+            {{ sections.plan_details.price }}
+          </h1>
+        </div>
+      </div>
+
+      <!-- descrições no header do plano -->
+      <div class="header-descriptions">
+        <p
+          v-for="(description, index) in sections.plan_details.description"
+          :key="index"
+          class="plan-short-description"
+        >
+          {{ description }}
+        </p>
+      </div>
+    </div>
+
+    <!-- indicação do plano -->
+    <div class="plan_indication">
+      <p class="plan_indication--text">
+        {{ sections.plan_details.indication }}
+      </p>
+    </div>
+
+    <!-- botão de ação -->
+    <div class="button_container">
+      <Button button_msg="Escolher esse plano" variant="cta" size="xs" />
+    </div>
+
+    <!-- descrições do plano -->
+    <div class="plan_benefits">
+      <div class="plan_benefits-title">
+        <h5>
+          {{ sections.plan_benefits.title
+          }}<span class="title_highlight">{{
+            sections.plan_benefits.title_highlight
+          }}</span>
+        </h5>
+      </div>
+      <div class="plan_benefits-items">
+        <p
+          v-for="(benefit, index) in sections.plan_benefits.benefits"
+          :key="index"
+          class="plan_item"
+        >
+          {{ benefit }}
+        </p>
+      </div>
+    </div>
+
+    <!-- suporte -->
+    <div class="plan_support">
+      <div class="plan_support-title">
+        <h5>{{ sections.plan_support }}</h5>
+      </div>
+    </div>
+
+    <!-- aplicativos disponveis -->
+    <div class="supported_apps">
+      <div class="supported_apps-title">
+        <h5>{{ sections.supported_apps.title }}</h5>
+      </div>
+      <div class="supported_apps-items">
+        <p
+          v-for="(app, index) in sections.supported_apps.appsList"
+          :key="index"
+          class="plan_item"
+        >
+          {{ app }}
+        </p>
+      </div>
+    </div>
+
+    <!--migrations-->
+    <div :v-if="sections.migrations" class="migration">
+      <div v-if="sections.migrations" class="migration-title">
+        <h5>
+          {{ sections.migrations && sections.migrations.title }}
+        </h5>
+      </div>
+      <div class="migration-items">
+        <p v-if="sections.migrations" class="plan_item">
+          {{ sections.migrations.description }}
+        </p>
+      </div>
+    </div>
+
+    <!-- benéficios extras -->
+    <div class="extra_benefits">
+      <div class="extra_benefits-title">
+        <h5>{{ sections.extra_benefits.title }}</h5>
+      </div>
+      <div class="extra_benefits-items">
+        <p
+          class="plan_item"
+          v-for="(extraBenefit, index) in sections.extra_benefits.benefits"
+          :key="index"
+        >
+          {{ extraBenefit }}
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
+<style scoped>
+  /* importa o css do componente */
+  @import "./style/PlanColStyle.css";
+</style>
---
+++ src/pages/PlansPage/components/PlanCol/interface/PlanColInterfaces.ts
@@ -0,0 +1,59 @@
+// Detalhes do plano
+type PlanDetails = {
+  title: string;
+  price: string;
+  indication: string;
+  description: string[];
+};
+
+// Benefícios do plano
+type PlanBenefits = {
+  title: string;
+  title_highlight?: string;
+  benefits: string[];
+};
+
+// Aplicativos suportados
+type SupportedApps = {
+  title: string;
+  appsList: string[];
+};
+
+// Migrações
+type Migrations = {
+  title?: string;
+  description?: string;
+};
+
+// Benefícios extras
+type ExtraBenefits = {
+  title: string;
+  benefits: string[];
+};
+
+// Plano
+type Plan = {
+  [x: string]: any;
+  plan_details: PlanDetails;
+  plan_benefits: PlanBenefits;
+  plan_support: string;
+  supported_apps: SupportedApps;
+  migrations?: Migrations;
+  extra_benefits: ExtraBenefits;
+};
+
+// Todos os planos
+type Plans = {
+  [plan: string]: Plan;
+};
+
+// Exportando interfaces
+export type {
+  PlanDetails,
+  PlanBenefits,
+  SupportedApps,
+  Migrations,
+  ExtraBenefits,
+  Plan,
+  Plans,
+};
