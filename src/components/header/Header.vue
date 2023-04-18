17c2693d30dfea3ca7de6306e08d4515de147ce4
Neto <ssneto88@gmail.com>
2023-04-18 20:11:40 -0300

    refact(header): relative paths and clean code practices 🔨

--- src/components/header/Header.vue
+++ src/components/header/Header.vue
@@ -1,23 +1,23 @@
 <script lang="ts">
   // importação dos componentes e bibliotecas
   import { defineComponent } from "vue";
-  import Logo from "../logo/Logo.vue";
-  import Slogan from "../slogan/Slogan.vue";
+  import Logo from "@/components/logo/Logo.vue";
+  import Slogan from "@/components/slogan/Slogan.vue";
 
   export default defineComponent({
     // propriedades do header
     props: {
-      title: {
+      cta: {
+        default: "",
         type: String,
-        default: "Você está muito próximo de mudar a forma de",
       },
       subtitle: {
-        type: String,
         default: "",
+        type: String,
       },
-      cta: {
+      title: {
+        default: "Você está muito próximo de mudar a forma de",
         type: String,
-        default: "",
       },
     },
     setup() {
@@ -32,7 +32,7 @@
   <div class="container__header">
     <!-- logo da locaweb -->
     <Logo />
-    <Slogan :title="title" :subtitle="subtitle" :cta="cta" />
+    <Slogan :cta="cta" :subtitle="subtitle" :title="title" />
   </div>
 </template>
 <style scoped>
