1d4cd2a3d898866bf15f111e352d09b78c9591bf
Neto <ssneto88@gmail.com>
2023-04-18 20:15:29 -0300

    refact(input): clean code practices 🔨

--- src/components/logo/Logo.vue
+++ src/components/logo/Logo.vue
@@ -2,11 +2,13 @@
   import { defineComponent } from "vue";
 
   export default defineComponent({
+    // importação dos componentes e bibliotecas
     setup() {
       return {};
     },
   });
 </script>
+
 <template>
   <!-- container do logo -->
   <div class="container__logo-plans--logobox">
@@ -18,6 +20,7 @@
     />
   </div>
 </template>
+
 <style scoped>
   /* importação do css */
   @import url("./style/LogoStyle.css");
