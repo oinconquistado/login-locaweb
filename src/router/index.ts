80331f35bbb99ccef79d0256aeb3cf9efd377f7f
Neto <ssneto88@gmail.com>
2023-04-18 17:02:12 -0300

    feat(routes): Meta tag

--- src/router/index.ts
+++ src/router/index.ts
@@ -7,9 +7,33 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     // Public
-    { path: "/", name: "Login", component: LoginPageVue },
-    { path: "/plans", name: "Plans", component: PlansPage },
-    { path: "/register/:id", name: "Register", component: RegistrationPage },
+    {
+      path: "/",
+      name: "Login",
+      component: LoginPageVue,
+      meta: {
+        requiresAuth: false,
+        title: "Login",
+      },
+    },
+    {
+      path: "/plans",
+      name: "Plans",
+      component: PlansPage,
+      meta: {
+        requiresAuth: false,
+        title: "Plans",
+      },
+    },
+    {
+      path: "/register/:id",
+      name: "Register",
+      component: RegistrationPage,
+      meta: {
+        requiresAuth: false,
+        title: "Register",
+      },
+    },
   ],
 });
 
